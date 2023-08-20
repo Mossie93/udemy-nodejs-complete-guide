d79fc076d1dd515e4f2ce24c08fcf26ca08c6d47
Rafal Mossakowski <rmossakowski@gmail.com>
2023-08-21 01:48:10 +0200

    Finish redirecting requests chapter
    Also fixed a typo - methd vs method

--- app.js
+++ app.js
@@ -6,23 +6,30 @@ const server = http.createServer((req, res) => {
   const method = req.method;
 
   res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>PAPAJ</title></head>')
 
   if (url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>PAPAJ</title></head>')
     res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form></body>')
+    res.write('</html>');
+    return res.end();
   }
 
-  if (url === '/message' && methd === 'POST') {
-    res.write('<body>Received a message!</body>')
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'DUMMY');
+    res.writeHead(302, {'Location': '/'});
+    return res.end();
   }
 
   if (url === '/message') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>PAPAJ</title></head>')
     res.write('<body>This will be a message page</body>')
+    res.write('</html>');
+    return res.end();
   }
-  
-  res.write('</html>');
-  res.end();
 });
 
 server.listen(3000);
