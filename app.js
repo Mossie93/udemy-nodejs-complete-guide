a45dbd532757861a10b171439e678cab90d06dbd
Rafal Mossakowski <rmossakowski@gmail.com>
2023-08-20 18:16:58 +0200

    Learned about routing

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const url = req.url;
+
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>PAPAJ</title></head>')
-  res.write('<b>21:37</b>')
+
+  if (url === '/') {
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form></body>')
+  }
+
+  if (url === '/message') {
+    res.write('<body>Received a message!</body>')
+  }
+  
   res.write('</html>');
   res.end();
 });
