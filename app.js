c86290a2d51b9763de65ef8630a4d4f2a8fd8be0
Rafal Mossakowski <rmossakowski@gmail.com>
2023-08-21 01:16:36 +0200

    WIP commit - need to save changes before going to sleep

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
 
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
@@ -11,8 +13,12 @@ const server = http.createServer((req, res) => {
     res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form></body>')
   }
 
-  if (url === '/message') {
+  if (url === '/message' && methd === 'POST') {
     res.write('<body>Received a message!</body>')
+  }
+
+  if (url === '/message') {
+    res.write('<body>This will be a message page</body>')
   }
   
   res.write('</html>');
